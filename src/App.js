587a4b0e722790beb77bd07773abea431d6a1552
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-11-04 19:25:14 +0600

    added footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Products from "./components/Products/Products";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Products></Products>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>Copy Right Reserved from Akib Rayhan</p>
+    </div>
+  );
+};
+
+export default Footer;
